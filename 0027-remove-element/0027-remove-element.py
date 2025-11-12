557e304bbddc7fd8c712e40c05acc3c013354a0d
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 19:00:39 +0530

    Time: 0 ms (100%), Space: 12.4 MB (80.09%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,9 +1,22 @@
+def verif(nums):
+    i=0
+    while(i<len(nums) and 0<=nums[i]<=50):
+        i+=1
+    return i==len(nums)
 class Solution(object):
     def removeElement(self, nums, val):
-       n=[x for x in nums if x!=val]
-       l=len(n)
-       for i in range(l):
-        nums[i]=n[i]
-       return l
-        
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        s=0
+        if(0<=len(nums)<=100 and 0<=val<=100 and verif(nums)):
+            i=0
+            while (i<len(nums)):
+                if(nums[i]==val):
+                    nums.remove(nums[i])
+                else:
+                    i+=1
+        return len(nums)
         
