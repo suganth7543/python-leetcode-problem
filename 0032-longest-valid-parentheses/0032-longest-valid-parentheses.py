be3174a1a626ed388730da1b64fc749124123945
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 19:01:41 +0530

    Time: 19 ms (76.3%), Space: 13.6 MB (73.55%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -4,36 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if len(s) == 0:
-            return 0
+        stack = [-1]
+        max_len = 0
 
-        # define DP as longest valid parentheses start at index i        
-        dp = [0] * len(s)
-
-        # first scan, pairs up all () as base case
-        for i in range(len(s)-1):
-            if s[i] == '(' and s[i+1] == ')':
-                dp[i] = 2
-
-        old_sum = 0
-        while sum(dp) > old_sum:                     
-            old_sum = sum(dp)      
-                  
-            for i in range(len(dp)):                
-                if i + dp[i] < len(dp):                     
-                    dp[i] = max(dp[i], dp[i] + dp[i+dp[i]])
-                    
-                if i > 0 and s[i-1] == '(' and i+dp[i] < len(dp) and s[i+dp[i]] == ')':
-                    dp[i-1] = max(dp[i-1], dp[i] + 2)
-
-        return max(dp)
-
-
-
-
-
-
-
-
-
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            else:
+                stack.pop()
+                if len(stack) == 0:
+                    stack.append(i)
+                else:
+                    max_len = max(max_len, i - stack[-1])
         
+        return max_len
