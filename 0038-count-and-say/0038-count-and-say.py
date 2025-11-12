ebf196428829162908c0f582b7e1f4b986f57fa8
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 19:02:49 +0530

    Time: 12 ms (30.94%), Space: 12.5 MB (49.36%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,22 +1,20 @@
-class Solution:
+class Solution(object):
     def countAndSay(self, n):
+        if n == 1:
+            return "1"
+
         result = "1"
 
         for _ in range(n - 1):
-            temp = ""
-            count = 1
-
-            for i in range(1, len(result)):
-                if result[i] == result[i - 1]:
+            new_result = ""
+            i = 0
+            while i < len(result):
+                count = 1
+                while i + 1 < len(result) and result[i] == result[i + 1]:
                     count += 1
-                else:
-                    temp += str(count) + result[i - 1]
-                    count = 1
-
-            temp += str(count) + result[-1]
-            result = temp
+                    i += 1
+                new_result += str(count) + result[i]
+                i += 1
+            result = new_result
 
         return result
-
-s = Solution()
-print(s.countAndSay(5))  
