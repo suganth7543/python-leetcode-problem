992421fb78ec81ec6474a6659c347c0f41041475
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 18:59:39 +0530

    Time: 36 ms (20.07%), Space: 18 MB (95.59%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,33 +1,31 @@
-from heapq import heappush, heappop
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class Solution:
+class Solution(object):
     def mergeKLists(self, lists):
-        if not lists or len(lists) == 0:
+        if not lists:
             return None
-        
-        heap = []
-        
-        # Step 1: Push the head of each list into heap
-        for i, node in enumerate(lists):
-            if node:
-                heappush(heap, (node.val, i, node))
-        
-        # Dummy head for the result linked list
-        dummy = ListNode(0)
-        current = dummy
-        
-        # Step 2: Pop smallest and push next node from that list
-        while heap:
-            val, i, node = heappop(heap)
-            current.next = node
-            current = current.next
-            if node.next:
-                heappush(heap, (node.next.val, i, node.next))
-        
+        if len(lists) == 1:
+            return lists[0]
+        mid = len(lists) // 2
+        l, r = self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
+        return self.merge(l, r)
+    
+    def merge(self, l, r):
+        dummy = p = ListNode()
+        while l and r:
+            if l.val < r.val:
+                p.next = l
+                l = l.next
+            else:
+                p.next = r
+                r = r.next
+            p = p.next
+        p.next = l or r
         return dummy.next
+    
+    def merge1(self, l, r):
+        if not l or not r:
+            return l or r
+        if l.val< r.val:
+            l.next = self.merge(l.next, r)
+            return l
+        r.next = self.merge(l, r.next)
+        return r
