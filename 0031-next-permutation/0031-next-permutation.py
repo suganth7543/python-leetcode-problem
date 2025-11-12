9e590b6e34932a7f05aa626828947521deeded86
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 19:01:29 +0530

    Time: 0 ms (100%), Space: 12.5 MB (13.14%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,33 +1,26 @@
 class Solution:
     def nextPermutation(self, nums):
         n = len(nums)
-        if n == 1:
-            return nums
+        idx = -1
 
-        def reverse(start, end):
-            while start < end:
-                nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
+        # Step 1: Find the pivot
+        for i in range(n - 1, 0, -1):
+            if nums[i] > nums[i - 1]:
+                idx = i - 1
+                break
 
-        def bs(start, end, k):
-            while start <= end:
-                mid = (start + end) // 2
-                if nums[mid] > k:
-                    start = mid + 1
-                else:
-                    end = mid - 1
-            return start - 1
+        # Step 2: Find the rightmost successor and swap
+        if idx != -1:
+            for i in range(n - 1, idx, -1):
+                if nums[i] > nums[idx]:
+                    nums[idx], nums[i] = nums[i], nums[idx]
+                    break
 
-        i = n - 2
-        while i >= 0:
-            if nums[i] < nums[i + 1]:
-                j = bs(i + 1, n - 1, nums[i])
-                nums[i], nums[j] = nums[j], nums[i]
-                reverse(i + 1, n - 1)
-                return nums
-            else:
-                i -= 1
+        # Step 3: Reverse the suffix
+        self.reverse(nums, idx + 1, n - 1)
 
-        reverse(0, n - 1)
-        return nums
+    def reverse(self, nums, i, j):
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
