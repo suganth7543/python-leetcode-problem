e4c243926865bd997c80c162a8462f0c6e6e2c79
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 19:02:35 +0530

    Time: 2073 ms (49.34%), Space: 12.4 MB (78.01%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,74 +1,35 @@
 class Solution(object):
-    def solveSudoku(self, board):
-        FULL_MASK = (1 << 9) - 1  
-        rows = [0] * 9
-        cols = [0] * 9
-        boxes = [0] * 9
-        empties = []
-
-        def box_index(r, c):
-            return (r // 3) * 3 + (c // 3)
-
-         
+    def solveSudoku(self,board):
+        R=[set()for _ in range(9)] #only 7 testcases here we can use list
+        C=[set()for _ in range(9)] #making 9 empty spaces in all 3
+        B=[set()for _ in range(9)]
+        E=[] #stores empty
         for r in range(9):
             for c in range(9):
-                if board[r][c] == '.':
-                    empties.append((r, c))
-                else:
-                    d = int(board[r][c]) - 1
-                    bit = 1 << d
-                    rows[r] |= bit
-                    cols[c] |= bit
-                    boxes[box_index(r, c)] |= bit
-
-        def dfs():
-            if not empties:
-                return True
-
-           
-            best_idx = -1
-            best_count = 10
-            best_mask = 0
-            for i, (r, c) in enumerate(empties):
-                mask = FULL_MASK & ~(rows[r] | cols[c] | boxes[box_index(r, c)])
-                count = bin(mask).count('1')   
-                if count < best_count:
-                    best_count = count
-                    best_idx = i
-                    best_mask = mask
-                    if count == 1:
-                        break
-
-            if best_count == 0:
-                return False
-
-            r, c = empties.pop(best_idx)
-            b = box_index(r, c)
-            mask = best_mask
-
-            while mask:
-                bit = mask & -mask
-                mask -= bit
-                d = bit.bit_length() - 1
-                board[r][c] = str(d + 1)
-
-                rows[r] |= bit
-                cols[c] |= bit
-                boxes[b] |= bit
-
-                if dfs():
-                    return True
-
-                rows[r] &= ~bit
-                cols[c] &= ~bit
-                boxes[b] &= ~bit
-                board[r][c] = '.'
-
-            empties.insert(best_idx, (r, c))
-            return False
-
-        dfs()
-        
-        
-         
-        
+                if board[r][c]==".":
+                    E.append((r,c))
+                else: #if element is not empty
+                    ch=board[r][c] #update in R,C,B lists
+                    R[r].add(ch) #add to row no.
+                    C[c].add(ch) #add to col no.
+                    B[(r//3)*3+(c//3)].add(ch) #add to block no
+        def f(k):
+            if k==len(E):
+                return 1 #for last index return True (1)
+            r,c=E[k]
+            b=(r//3)*3+(c//3) #block no.
+            for ch in"123456789":
+                if ch not in R[r]and ch not in C[c]and ch not in B[b]: #checking not in all
+                    board[r][c]=ch #adding to the cell
+                    R[r].add(ch)
+                    C[c].add(ch) #update in all R,C,B lists of this index 
+                    B[b].add(ch)
+                    if f(k+1):  #checking if the next is last element
+                        return 1
+                    board[r][c]="."
+                    R[r].remove(ch)
+                    C[c].remove(ch) #claning the records
+                    B[b].remove(ch)
+            return 0
+        f(0)
+                  
