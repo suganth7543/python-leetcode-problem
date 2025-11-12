fccf36d4ebd904710d23ef8d09b2ac880092eb56
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 19:02:24 +0530

    Time: 0 ms (100%), Space: 12.4 MB (55.25%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,25 +4,29 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        columns = [[board[r][c] for r in range(9)] for c in range(9)]
-        for i in range(9):
-            temp=[k for k in board[i] if k!='.']
-            if len(temp)!=len(set(temp)):
-                return False
-            temp=[k for k in columns[i] if k!='.']
-            if len(temp)!=len(set(temp)):
-                return False
-        i=0
-        temp=[]
-        while(i<9):
-            j=0
-            while j<9:
-                new=board[i][j:j+3]
-                new.extend(board[i+1][j:j+3])
-                new.extend(board[i+2][j:j+3])
-                temp=[k for k in new if k!='.']
-                if len(temp)!=len(set(temp)):
+        #Sets for the validation
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        sqr = [set() for _ in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+                current = board[r][c] #get the current cell
+
+                if current == '.':
+                    continue
+
+                #check the value for every set
+                if current in rows[r]:
                     return False
-                j=j+3
-            i=i+3
+                rows[r].add(current)
+
+                if current in cols[c]:
+                    return False
+                cols[c].add(current)
+
+                if current in sqr[(r//3)*3+(c//3)]:
+                    return False
+                sqr[(r//3)*3+(c//3)].add(current)
+                
         return True
