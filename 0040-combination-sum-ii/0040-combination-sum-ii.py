99829d2ebed662c324e825866258191c5788afce
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 19:04:26 +0530

    Time: 19 ms (70.53%), Space: 12.4 MB (94.66%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,25 +1,22 @@
-class Solution:
+class Solution(object):
     def combinationSum2(self, candidates, target):
-        candidates.sort()  # sort to help skip duplicates
-        result = []
-
-        def backtrack(start, path, total):
-            if total == target:
-                result.append(list(path))
+        # O(2^n)
+        # O(n)
+        output = []
+        candidates.sort()
+        def backtract(i, path, pathSum):
+            if pathSum == target:
+                output.append(list(path))
                 return
-            if total > target:
-                return
+            
             prev = -1
-            for i in range(start, len(candidates)):
-                if candidates[i] == prev:
-                    continue  # skip duplicates
-                path.append(candidates[i])
-                backtrack(i + 1, path, total + candidates[i])  # move to next index
-                path.pop()
-                prev = candidates[i]
+            for j in range(i, len(candidates)):
+                if pathSum + candidates[j] > target or candidates[j] == prev:
+                    continue
+                    
+                backtract(j + 1, path + [candidates[j]], pathSum + candidates[j])
+                prev = candidates[j]
+            
+        backtract(0, [], 0)
 
-        backtrack(0, [], 0)
-        return result
-sol = Solution()
-print(sol.combinationSum2([10,1,2,7,6,1,5], 8))
-# Output: [[1,1,6],[1,2,5],[1,7],[2,6]]
+        return output
