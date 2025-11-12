bd412cb8394b27dff51da7af10d3b1533b57a655
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 18:56:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,21 +1,7 @@
-class Solution:
-    def generate(self,curr_str,ans,Open,Close,n):
-        if(Open==Close and Open+Close==2*n):
-            ans.append(curr_str)
-            return
-        if(Open>n):
-            return
-        if(Close>Open):
-            return    
-        self.generate(curr_str+"(",ans,Open+1,Close,n)
-        self.generate(curr_str+")",ans,Open,Close+1,n)
-    def generateParenthesis(self,n):
-        curr_str=""
-        ans=[]
-        Open=0
-        Close=0
-        self.generate(curr_str,ans,Open,Close,n)
-        return ans
-obj=Solution()
-n=2
-print(obj.generateParenthesis(n))                    
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        
