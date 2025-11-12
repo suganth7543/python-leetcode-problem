fc3a73269edb952c5b6ff8d7fe8587565dee12de
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 19:01:07 +0530

    Time: 0 ms (100%), Space: 12.5 MB (12.83%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,17 +1,25 @@
 class Solution(object):
-    def divide(self, dividend, divisor):
-        sign = 1
-        if divisor < 0 or dividend < 0:
-            sign = -1
-        if divisor < 0 and dividend < 0:
-            sign = 1
+    def divide(self, dividend, divisor): # 10, 3
+        a, b = dividend, divisor # 10, 3
+        if a == 0: return 0
 
-        divisor = abs(divisor)
-        dividend = abs(dividend)
+        sign = -1 if (a < 0) ^ (b < 0) else 1 # 1
+        a = abs(a) # 10
+        b = abs(b) # 3
+        
+        quotient = 0
+        temp = b # 3
+        while a >= (temp << 1): # 2**32 >= 2 ** 33
+            temp <<= 1 # 2 ** 32
+        
+        while temp >= b: # 2 ** 31 >= 2**0
+            if a >= temp: # 0 >= 2 ** 32
+                a -= temp # 0
+                quotient += 1 # 1
+            temp >>= 1 # 2 ** 31
+            quotient <<= 1 # 2 ** 32
 
-        if sign * (dividend // divisor) > (2**31 - 1):
-            return 2**31 - 1
-        if sign * (dividend // divisor) < -(2**31):
-            return -2**31
-        else:
-            return sign * (dividend // divisor)
+        res = quotient * sign >> 1
+        maxi = (2**31) - 1
+        return res if res <= maxi else maxi
+        
