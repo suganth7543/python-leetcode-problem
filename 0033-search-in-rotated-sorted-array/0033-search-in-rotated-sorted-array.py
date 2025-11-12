6a619b8d916466d6f848327fbfe2341e5f3cfab1
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 19:01:51 +0530

    Time: 0 ms (100%), Space: 12.7 MB (42.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,8 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        try:
-            return nums.index(target)
-        except (Exception):
-            return -1
+        l,r=0,len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return mid
+            if nums[mid]>=nums[l]:
+                if nums[l]<=target<=nums[mid]:
+                    r=mid-1
+                else:
+                    l=mid+1
+            else:
+                if nums[mid]<=target<=nums[r]:
+                    l=mid+1
+                else:
+                    r=mid-1
+        return -1
         
