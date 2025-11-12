59b2b3c1f7302eccace5dff28397894282a7949f
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 19:02:13 +0530

    Time: 0 ms (100%), Space: 12.9 MB (88.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,10 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        for i in range(len(nums)+1):
-            if(i < len(nums)):
-                if (nums[i] >= target):
-                    return i
-            if(i == len(nums)):
-                return i
-            
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
