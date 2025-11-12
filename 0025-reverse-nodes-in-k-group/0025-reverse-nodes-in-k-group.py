7d5791521df48a9efc935fb7ce3b81a908c5020d
suganth7543 <suganthsakthivel36@gmail.com>
2025-11-12 19:00:15 +0530

    Time: 4 ms (29.27%), Space: 14.3 MB (45.09%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -1,52 +1,21 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
+class Solution:
+    def reverse(self, start, end):
+        prev, curr = None, start
+        while curr != end:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+
     def reverseKGroup(self, head, k):
-        """
-        :type head: Optional[ListNode]
-        :type k: int
-        :rtype: Optional[ListNode]
-        """
-        head2=None
-        head1=tail=head
-        prev_tail=None
-        
-        def rev(start,end):
-            cur=start
-            prev=None
+        count, temp = 0, head
+        while temp and count < k:
+            temp = temp.next
+            count += 1
+        if count < k:
+            return head
 
-            while cur!=end:
-                tmp=cur.next
-                cur.next=prev
-                prev=cur
-                cur=tmp
-            start.next=end
-            
-            return prev
-
-        
-        while True:
-            tmp=tail
-            for i in range(k):
-                if tmp:
-                    tmp=tmp.next
-                else:
-                    return head2 if head2 else head
-            
-            tail=tmp
-            new_head=rev(head1,tail)
-
-            if head2 is None:
-                head2=new_head
-            
-            if prev_tail:
-                prev_tail.next=new_head
-
-            prev_tail=head1
-            head1=tail
-        return head2
-            
-                
+        new_head = self.reverse(head, temp)
+        head.next = self.reverseKGroup(temp, k)
+        return new_head
